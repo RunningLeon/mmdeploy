dd55344d62657ecaf147c3efd2a765448d2074de
RunningLeon <mnsheng@yeah.net>
2022-11-22 12:22:22 +0800

    select configs

--- configs/mmseg/segmentation_static.py
+++ configs/mmseg/segmentation_static.py
@@ -1,2 +1,10 @@
 _base_ = ['../_base_/onnx_config.py']
 codebase_config = dict(type='mmseg', task='Segmentation')
+onnx_config = dict(dynamic_axes={
+    'input': {
+        0: 'batch',
+    },
+    'output': {
+        0: 'batch',
+    },
+})
